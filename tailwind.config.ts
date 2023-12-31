9aa30fb41887ad7b32342b352ebd3e16e20f0deb
Peterdinis <peterdinis0@pobox.sk>
2023-12-31 22:44:37 +0100

    fix: Fixing small bug in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "tailwindcss"
-const {nextui} = require("@nextui-org/react");
 
 const config = {
   darkMode: ["class"],
@@ -7,8 +6,7 @@ const config = {
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
     './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{ts,tsx}'
 	],
   prefix: "",
   theme: {
@@ -76,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"), nextui()],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config
 
 export default config
