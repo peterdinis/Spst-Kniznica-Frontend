cb5b83ec071210e95d4766efb2add5136f9ac5b8
Peterdinis <peterdinis0@pobox.sk>
2024-01-02 17:31:00 +0100

    feat: Custom query-client WIP

--- src/api/QueryClientProviderComponent.tsx
+++ src/api/QueryClientProviderComponent.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { FC, ReactNode } from 'react';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider} from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { queryClient } from './queryClient';
 
 interface IQueryClientProps {
     children?: ReactNode;
@@ -11,8 +12,6 @@ interface IQueryClientProps {
 const QueryClientProviderComponet: FC<IQueryClientProps> = ({
     children,
 }: IQueryClientProps) => {
-    const queryClient = new QueryClient();
-
     return (
         <QueryClientProvider client={queryClient}>
             {children}
