fc19c532e0d913daa1fda2a37afad0dbce27c686
Peterdinis <peterdinis0@pobox.sk>
2024-01-02 19:39:46 +0100

    feat: Displaying categories

--- src/app/components/categories/AllCategories.tsx
+++ src/app/components/categories/AllCategories.tsx
@@ -4,17 +4,46 @@ import { FC } from 'react';
 import Header from '../shared/Header';
 import ScrollToTop from 'react-scroll-to-top';
 import GlobalCard from '../shared/GlobalCard';
+import * as api from "@/api/queries/categoryQueries"
+import { useQuery } from '@tanstack/react-query';
+import { Loader2 } from 'lucide-react';
+import GlobalErrorComponent from '../shared/GlobalErrorComponent';
+import { CategoryType } from '@/types/CategoryTypes';
 
 const AllCategories: FC = () => {
+    const {data, isLoading, isError} = useQuery({
+        queryKey: ["allCategories"],
+        queryFn: api.allCategories
+    });
+
+    if (isLoading) {
+        return <Loader2 className='h-8 w-8 animate-spin' />;
+    }
+
+    if (isError) {
+        return (
+            <GlobalErrorComponent
+                statusCode='400'
+                message='Failed to load books'
+            />
+        );
+    }
+
     return (
         <>
             <Header text='Všetky kategórie' />
             <div className='container mx-auto mt-8 grid gap-8 pt-6 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-                <GlobalCard />
-                <GlobalCard />
-                <GlobalCard />
-                <GlobalCard />
-                <GlobalCard />
+                {data &&
+                    data.map((item: CategoryType) => {
+                        return (
+                            <GlobalCard
+                                linkName='categories'
+                                name={item.name}
+                                description={item.description}
+                                id={item.id}
+                            />
+                        );
+                    })}
             </div>
             <ScrollToTop />
         </>
