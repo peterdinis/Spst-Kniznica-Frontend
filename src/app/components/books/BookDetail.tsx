8fe24d6016bc106fa3e37e4a5c18ff84f5437be2
Peterdinis <peterdinis0@pobox.sk>
2024-01-03 17:12:17 +0100

    feat: Book detail page done

--- src/app/components/books/BookDetail.tsx
+++ src/app/components/books/BookDetail.tsx
@@ -1,85 +1,127 @@
+"use client"
+
 import { FC } from 'react';
 import Header from '../shared/Header';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import { useParams } from 'next/navigation';
 import BookingModal from '../booking/BookingModal';
+import * as api from '../../../api/queries/bookQueries';
+import { useQuery } from '@tanstack/react-query';
+import GlobalErrorComponent from '../shared/GlobalErrorComponent';
+import { Loader2 } from 'lucide-react';
 
 const BookDetail: FC = () => {
+    const { id } = useParams();
+
+    const { data, isLoading, isError } = useQuery({
+        queryKey: ['bookDetail', id],
+        queryFn: () => api.getOneBook(id as unknown as number),
+    });
+
+    if (isError) {
+        return (
+            <GlobalErrorComponent
+                statusCode='404'
+                message='Kniha nebola nájdená'
+            />
+        );
+    }
     return (
         <>
             <Header text='Detail knihy' />
-            <section className='body-font mt-2 overflow-hidden bg-white text-gray-700'>
-                <div className='container mx-auto px-5 py-12'>
-                    <div className='mx-auto flex flex-wrap lg:w-4/5'>
-                        <img
-                            alt='ecommerce'
-                            className='w-full rounded-lg border object-cover object-center drop-shadow-md lg:w-1/2'
-                            src='https://picsum.photos/200/300'
-                        />
-                        <div className='mt-6 w-full lg:mt-0 lg:w-1/2 lg:py-6 lg:pl-10'>
-                            <div>
-                                <h1 className='title-font mb-1 text-4xl font-medium text-gray-900'>
-                                    <span className='font-bold'>Názov</span>:{' '}
-                                    abc
-                                </h1>
-                            </div>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'>rrrrr</span>
-                            </p>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'>Autor / ka</span>:{' '}
-                                rrrrr
-                            </p>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'> Rok</span>: 2222
-                            </p>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'> Počet Strán</span>:{' '}
-                                2222
-                            </p>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'>Počet Kusov</span>:{' '}
-                                1
-                            </p>
-                            <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
-                                <span className='font-bold'>Kategória</span>:{' '}
-                                abc
-                            </p>
-                            <div>
-                                <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed'>
-                                    <span className='font-bold'>
-                                        {' '}
-                                        Kniha je:
-                                    </span>{' '}
-                                    <span className='text-red-800'>
-                                        Nedostupná
-                                    </span>
-                                    <br />
-                                </p>
-                            </div>
+            {isLoading && <Loader2 className='h-6 w-6 animate-spin' />}
 
-                            <hr className='mt-6' />
-                            <div className='flex'>
-                                <Button
-                                    variant={'default'}
-                                    size={'lg'}
-                                    className='mt-5'
-                                >
-                                    <Link href='/books'>Návrat na knihy</Link>
-                                </Button>
-                                <Button
-                                    variant={'secondary'}
-                                    size={'lg'}
-                                    className='ml-4 mt-5'
-                                >
-                                    <BookingModal />
-                                </Button>
+            {!isLoading ? (
+                <>
+                    <section className='body-font mt-2 overflow-hidden bg-white text-gray-700'>
+                        <div className='container mx-auto px-5 py-12'>
+                            <div className='mx-auto flex flex-wrap lg:w-4/5'>
+                                <img
+                                    alt={data && data.name}
+                                    className='w-full rounded-lg border object-cover object-center drop-shadow-md lg:w-1/2'
+                                    src={data &&data.image}
+                                />
+                                <div className='mt-6 w-full lg:mt-0 lg:w-1/2 lg:py-6 lg:pl-10'>
+                                    <div>
+                                        <h1 className='title-font mb-1 text-4xl font-medium text-gray-900'>
+                                            <span className='font-bold'>
+                                                Názov
+                                            </span>
+                                            : {data && data.name}
+                                        </h1>
+                                    </div>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'>Krátky popis: {data && data.description}</span>
+                                    </p>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'>
+                                            Autor / ka
+                                        </span>
+                                        : {data && data.authorName}
+                                    </p>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'> Rok</span>:
+                                        {data && data.year}
+                                    </p>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'>
+                                            {' '}
+                                            Počet Strán
+                                        </span>
+                                        : {data &&data.description}
+                                    </p>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'>
+                                            Počet Kusov
+                                        </span>
+                                        : {data && data.quantity}
+                                    </p>
+                                    <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed text-gray-800'>
+                                        <span className='font-bold'>
+                                            Kategória
+                                        </span>
+                                        : rrrr
+                                    </p>
+                                    <div>
+                                        <p className='mb-4 mt-3 text-2xl font-light  leading-relaxed'>
+                                            <span className='font-bold'>
+                                                {' '}
+                                                Kniha je:
+                                            </span>{' '}
+                                            <span className='text-red-800'>
+                                               {data && data.status}
+                                            </span>
+                                            <br />
+                                        </p>
+                                    </div>
+
+                                    <hr className='mt-6' />
+                                    <div className='flex'>
+                                        <Button
+                                            variant={'default'}
+                                            size={'lg'}
+                                            className='mt-5'
+                                        >
+                                            <Link href='/books'>
+                                                Návrat na knihy
+                                            </Link>
+                                        </Button>
+                                        <Button
+                                            variant={'secondary'}
+                                            size={'lg'}
+                                            className='ml-4 mt-5'
+                                        >
+                                            <BookingModal />
+                                        </Button>
+                                    </div>
+                                    <br />
+                                </div>
                             </div>
-                            <br />
                         </div>
-                    </div>
-                </div>
-            </section>
+                    </section>
+                </>
+            ) : null}
         </>
     );
 };
