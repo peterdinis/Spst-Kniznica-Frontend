bb31e50b31429547abc693cd7dc8f116f3fb0bcc
Peterdinis <peterdinis0@pobox.sk>
2024-01-03 17:49:53 +0100

    style: Small changes in globalCard

--- src/app/components/shared/GlobalCard.tsx
+++ src/app/components/shared/GlobalCard.tsx
@@ -2,12 +2,10 @@ import { FC } from 'react';
 import {
     Card,
     CardDescription,
-    CardFooter,
     CardHeader,
     CardTitle,
 } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-import { MoveRight } from 'lucide-react';
 import { IGlobalCardType } from '@/types/GlobalTypes';
 import Link from 'next/link';
 
@@ -20,18 +18,19 @@ const GlobalCard: FC<IGlobalCardType> = ({
 }) => {
     return (
         <Card>
-            <img src={image as unknown as string} alt={name as unknown as string} />
+            <img
+                src={image as unknown as string}
+                alt={name as unknown as string}
+            />
             <CardHeader>
                 <CardTitle>{name}</CardTitle>
                 <CardDescription>{description}</CardDescription>
             </CardHeader>
-            <CardFooter>
-                <Button  variant={'default'} size='lg'>
-                    <Link href={`/${linkName}/${id}`}>
-                        Detail <MoveRight className='ml-2' />
-                    </Link>
-                </Button>
-            </CardFooter>
+            <Button variant={'default'} size='lg'>
+                <Link href={`/${linkName}/${id}`}>
+                    Detail 
+                </Link>
+            </Button>
         </Card>
     );
 };
